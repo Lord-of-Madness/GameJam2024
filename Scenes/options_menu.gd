488601534135e925c79162cd6258f89cfcb51365
Michal Tomášek <michal.tomasek6@seznam.cz>
2024-12-01 00:13:19 +0100

    option menu still broken

--- Scenes/options_menu.gd
+++ Scenes/options_menu.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 func _ready() -> void:
-	$Button.grab_focus()
+	$TurnOff.grab_focus()
 	
 
 func _on_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 
-func _on_button_pressed() -> void:
+func _on_turn_off_pressed() -> void:
 	MusicManager.mute()
