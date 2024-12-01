993c08a37de57daca9290146b32d74fc6e3d2820
Petr Kotáb <petrkotab99@gmail.com>
2024-12-01 12:30:08 +0100

    added day night counter animation during day night switch animation

--- Scenes/UI/day_night_counter.gd
+++ Scenes/UI/day_night_counter.gd
@@ -1,6 +1,8 @@
 class_name DayNightCounter
 extends Control
 
+var DAY_NIGHT_SWITCH_DURATION := 0.75
+
 var _number := 0
 var _day := false
 
@@ -8,14 +10,27 @@ func _ready() -> void:
 	$Moon.hide()
 
 func switch_day_night():
-	if _day:
-		$Moon.show()
-		$Sun.hide()
-	else:
-		$Moon.hide()
-		$Sun.show()
+	if _number == 0 and !_day:
+		#_day = !_day
+		return
+	
+	$RotationCenter/Moon.show()
+	$RotationCenter/Sun.show()
+	
+	var tween := get_tree().create_tween()
+	var target_rotation: float = $RotationCenter.rotation + PI / 2.0
+	tween.tween_property($RotationCenter, "rotation", target_rotation, DAY_NIGHT_SWITCH_DURATION)
+	#tween.tween_callback(_apply_hide)
 		
 	_day = !_day
+
+#func _apply_hide():
+	#if _day:
+		#$RotationCenter/Moon.show()
+		#$RotationCenter/Sun.hide()
+	#else:
+		#$RotationCenter/Moon.hide()
+		#$RotationCenter/Sun.show()
 
 func increment():
 	_number += 1
@@ -28,4 +43,7 @@ func reset():
 	switch_day_night()
 	
 func on_number_changed():
-	$Label.text = str(_number)
+	$RotationCenter/Sun/Label.text = str(_number)
+	$RotationCenter/Moon/Label.text = str(_number)
+	$RotationCenter/Sun2/Label.text = str(_number)
+	$RotationCenter/Moon2/Label.text = str(_number)
