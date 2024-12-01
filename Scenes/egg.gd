2168686021adbd32b28042736d3d0085935bf5da
Petr Kotáb <petrkotab99@gmail.com>
2024-12-01 10:55:09 +0100

    resource pickup effects

--- Scenes/egg.gd
+++ Scenes/egg.gd
@@ -10,6 +10,7 @@ func _process(delta: float) -> void:
 		MusicManager.interact.play()
 		PlayerData.increment_egg_count()
 		queue_free()
+		RewardEffect.new_egg()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	is_player_in_range = true
